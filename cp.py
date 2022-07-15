ddead8f137c4628b08a1ea40f2c119b535e9dad2
Jordi Malaret <jamwood111@gmail.com>
2022-07-15 08:38:23 -0700

    CP with doc

---
+++ cp.py
@@ -0,0 +1,28 @@
+import numpy as np
+import random
+
+def clip_producer(audio_samp : np.ndarray, l : int):
+    """
+    Function that takes long arrays of digital samples (eg. 2 min long audio) 
+    and returns a random portion of a determined size. 
+    This can be used to test small clips on the song identification program
+
+    Parameter 
+    ---------------
+    audio_samp : numpy.ndarray
+        array of digital audio samples
+    l : int
+        length of clip produced in seconds
+    
+    Returns
+    -------
+    rand_clip : numpy.ndarray
+        random portion of audio_samp array of shape((44100 * l),)
+    """
+    data = audio_samp
+    split_amt = data.shape[0]//(44100 * l)
+    split_data = np.array_split(data, split_amt)
+    rand_clip = random.choice(split_data)
+
+
+    return rand_clip
