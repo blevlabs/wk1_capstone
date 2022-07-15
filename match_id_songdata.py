049b04fabd0562ad32034711ec646899a36845c0
brann2264 <2264bran@gmail.com>
2022-07-15 12:34:02 -0400

    some changes to docstring

--- match_id_songdata.py
+++ match_id_songdata.py
@@ -1,4 +1,12 @@
 def match_id_songdata(song_id:int):
+    """
+    Parameter:
+        song_id: int
+    
+    Return:
+        str: (song_name + "by " + artist)
+    """
+    
     dict = "dictionary of already saved ids"
     song_name, artist = dict[song_id]
-    return print(song_name + "by " + artist)
+    return str(song_name + "by " + artist)
